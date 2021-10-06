60f33197ef5ddd08b320fe741cf7f926bf50124e
SamundeeswariAnadhan <91953293+SamundeeswariAnadhan@users.noreply.github.com>
2021-10-06 11:01:37 +0530

    Update department.java

--- src/jdbccon/department.java
+++ src/jdbccon/department.java
@@ -1,55 +1,52 @@
 package jdbccon;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
-public class department {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/chami";
-    static final String USER ="chami";
-    static final String PASS ="1234";
+public class connectDepStu
+{
+	
+	static final String DB_URL = "jdbc:mysql://localhost:3306/chami";
+	static final String USER ="chami";
+	static final String PASS ="1234";
+	
+	public static void main(String[] args) 
+	{
+	try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		     Statement stmt = conn.createStatement();
+		) {
+		    Statement st = conn.createStatement();
+		    ResultSet res = st.executeQuery("select studID, FirstName, LastName,joiningDate,DOB,MobileNo,EmailID, Department\n" +
+		            "from student \n" +
+		            "join Department \n" +
+		            "On student.DepID = Department.DepID");
+		    System.out.println("stuID" + " FirstName " + "LastName"+ "joiningDate"+ "DOB"+"MobileNo"+"EmailID"+ "DepID" + "Department");
+		    while (res.next()) {
+		        String stuID = res.getString("studID");
+		        String FirstName = res.getString("FirstName");
+		        String LastName = res.getString("LastName");
+		        String joiningDate = res.getString("joiningDate");
+		        String DOB = res.getString("DOB");
+		        String MobileNo = res.getString("MobileNo");
+		        String EmailID = res.getString("EmailID");
+		        
+		        //String DepID = res.getString("DepID");
+		        String Department = res.getString("Department");
+		     
+		        System.out.format(
+		                "%10s%10s%10s%10s%10s%20s%20s%20s\n",stuID, FirstName, LastName, joiningDate, DOB,MobileNo,EmailID ,Department);
+		    
+		    }
 
-    public static void main(String[] args) {
-
-        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement();
-        ) {
-            String drops = "DROP TABLE IF EXISTS department";
-            stmt.executeUpdate(drops);
-
-
-            String sql = "CREATE TABLE IF NOT EXISTS DEPARTMENT " +
-                    "(DepID smallint , " +
-                    " Department VARCHAR(30), " +
-                    " PRIMARY KEY ( DepID ))";
-
-            stmt.executeUpdate(sql);
-            System.out.println("table created for department table");
-
-
-            System.out.println("Inserting records into the table...");
-            sql = "INSERT INTO Department VALUES (111, 'CSE')";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Department VALUES (112, 'EEE')";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Department VALUES (113, 'IT')";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Department VALUES(114, 'ECE')";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Department VALUES(115, 'Arch')";
-            stmt.executeUpdate(sql);
-            sql = "INSERT INTO Department VALUES(116, 'MECH')";
-            stmt.executeUpdate(sql);
-
-            System.out.println("data inserted");
-
-        } catch (SQLException e) {
+            // Step 7: Close the connection
+            conn.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-}
+}	    
+	
 
 
 
