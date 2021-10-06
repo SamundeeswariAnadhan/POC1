3f144265df9228ea8077b3cfe6b21ee66a6b11a1
SamundeeswariAnadhan <91953293+SamundeeswariAnadhan@users.noreply.github.com>
2021-10-06 11:06:53 +0530

    Update connectDepStu.java

--- src/jdbccon/connectDepStu.java
+++ src/jdbccon/connectDepStu.java
@@ -4,33 +4,38 @@ import java.sql.*;
 
 public class connectDepStu
 {
-
-    static final String DB_URL = "jdbc:mysql://localhost:3306/chami";
-    static final String USER ="chami";
-    static final String PASS ="1234";
-
-    public static void main(String[] args)
-    {
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-             Statement stmt = conn.createStatement();
-        ) {
-            Statement st = conn.createStatement();
-            ResultSet res = st.executeQuery("select s.ID, s.FirstName, s.LastName, s.DeptID, d.Deptname\n" +
-                    "from students s\n" +
-                    "join Department d\n" +
-                    "On s.DeptID = d.DeptID");
-            System.out.println("ID" + " FirstName " + "LastName" + "Deptname" + "DeptID");
-            while (res.next()) {
-                String ID = res.getString("ID");
-                String FirstName = res.getString("FirstName");
-                String LastName = res.getString("LastName");
-                String Deptname = res.getString("Deptname");
-                String DeptID = res.getString("DeptID");
-                // Beautification of output
-                System.out.format(
-                        "%10s%10s%10s%10s%20s\n", ID, FirstName, LastName, Deptname, DeptID);
-
-            }
+	
+	static final String DB_URL = "jdbc:mysql://localhost:3306/chami";
+	static final String USER ="chami";
+	static final String PASS ="1234";
+	
+	public static void main(String[] args) 
+	{
+	try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		     Statement stmt = conn.createStatement();
+		) {
+		    Statement st = conn.createStatement();
+		    ResultSet res = st.executeQuery("select studID, FirstName, LastName,joiningDate,DOB,MobileNo,EmailID, Department\n" +
+		            "from student \n" +
+		            "join Department \n" +
+		            "On student.DepID = Department.DepID");
+		    System.out.println("stuID" + " FirstName " + "LastName"+ "joiningDate"+ "DOB"+"MobileNo"+"EmailID"+ "DepID" + "Department");
+		    while (res.next()) {
+		        String stuID = res.getString("studID");
+		        String FirstName = res.getString("FirstName");
+		        String LastName = res.getString("LastName");
+		        String joiningDate = res.getString("joiningDate");
+		        String DOB = res.getString("DOB");
+		        String MobileNo = res.getString("MobileNo");
+		        String EmailID = res.getString("EmailID");
+		        
+		        //String DepID = res.getString("DepID");
+		        String Department = res.getString("Department");
+		     
+		        System.out.format(
+		                "%10s%10s%10s%10s%10s%20s%20s%20s\n",stuID, FirstName, LastName, joiningDate, DOB,MobileNo,EmailID ,Department);
+		    
+		    }
 
             // Step 7: Close the connection
             conn.close();
@@ -40,7 +45,8 @@ public class connectDepStu
             e.printStackTrace();
         }
     }
-}
+}	    
+	
 
 
 
